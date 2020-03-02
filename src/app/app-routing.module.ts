90aa5df4a85bacb3317f30ea6fe74424adce4705
Harman Singh <36654923+hrmnsngh@users.noreply.github.com>
2020-03-02 17:24:23 +0530

    Updating Complete Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { WelcomeComponent } from './home/welcome.component';
-import { ProductListComponent } from './products/product-list.component';
-import { MessageComponent } from './messages/message.component';
-import { LoginComponent } from './user/login.component';
+
+/* Feature Modules */
+
+import { ProductData } from './products/product-data';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { WelcomeComponent } from './home/welcome.component';
+
+const Routes = [
+    {path: 'welcome', component: WelcomeComponent },
+    { path: 'products', component: ProductData},
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot([
-            { path: 'home', component: WelcomeComponent },
-            { path: 'welcome', redirectTo: 'home', pathMatch: 'full'},
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'products', component: ProductListComponent},
-            { path: 'messages', component: MessageComponent},
-            { path: 'login', component: LoginComponent},
-            { path: '**', component: PageNotFoundComponent }
-          ], {useHash: true})
-    ],
-   exports: [RouterModule]
+    imports: [RouterModule.forRoot(Routes)],
+    exports: [RouterModule]
 })
 
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { ProductData } from './products/product-data';
 
-import { AppComponent } from './app.component';
-import { WelcomeComponent } from './home/welcome.component';
-import { PageNotFoundComponent } from './page-not-found.component';
-
-/* Feature Modules */
 import { ProductModule } from './products/product.module';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
+import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { WelcomeComponent } from './home/welcome.component';
+
+
 
 @NgModule({
   imports: [
